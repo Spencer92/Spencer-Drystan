3192223e8f13d9e477b7cc4a8b14cf6d2530dfeb
gloopy <drystan.mazur@gmail.com>
2015-02-12 21:57:48 -0700

    new update

--- gameEngine/src/GameEngine.c
+++ gameEngine/src/GameEngine.c
@@ -12,35 +12,51 @@
  *Description : Raster library for Atarti St, ANSI-style C89
  *Source File : testLib.c
  =============================================================================
- Purpose      : To demonstate a working game model
+ Purpose      : To demonstrate a working game model
  =============================================================================
  Method       :
 
  ============================================================================ */
 
-
 #include <osbind.h>
 #include <stdio.h>
 #include <stdlib.h>
 /*===================================================*/
+/*Header files authored by Others*/
 #include "font.h"
 #include  "types.h"
 /*===================================================*/
-#include "raster.h"
+/*Header files authored by Spencer Maslen or Drystan Mazur*/
+
+
 #include  "model.h"
-#include "clrScrn.h"
+#include  "clrScrn.h"
 #include  "Keyboard.h"
 #include  "AIsystem.h"
 #include  "CollsionDetect.h"
 #include  "TileControl.h"
-#include
+#include  "RenderEngine.h"
+
 /*===================================================*/
+#define  NUMBER_OFLIVES 3
+#define  PLAYER_HITPOINTS 50
+#define  ENEMY_TANKHITPOINTS 5
+#define  NUMBER_OFENEMYTANKS 5
+#define  NUMBER_TILES 1
+
+
+void gameReset();
 
 
 
+int main() {
 
-int main()
-{
+/*Intaiate game */
+
+
 
 
 }
+
+void gameReset()
+{}
--- gameEngine/src/RenderEngine.h
+++ gameEngine/src/RenderEngine.h
@@ -2,5 +2,28 @@
 #define RENDERENGINE_H
 
 
+#include "types.h"
+
+
+
+
+void plotPixel(char *fbstart, int x, int y);
+
+void plotHorzLine(char *fbstart, int xstart, int ystart, int xfin);
+
+void plotVertLine(char *fbstart, int xstart, int ystart, int yfin);
+
+void plotArbLine(char *fbstart, int xstart, int ystart, int xfin, int yfin);
+
+void plotSprite(char *fbstart, UINT8 *spriteLocation, int xpostoPlot, int ypostoPlot, int size );
+
+void printvertical(unsigned char* base, unsigned int x_loc, unsigned int y_loc,
+                   unsigned int length);
+
+void printArbitrary(unsigned char* base, unsigned int x_loc_start,
+                    unsigned int y_loc_start, unsigned int x_loc_end,
+                    unsigned int y_loc_end);
+
+#endif /* RASTER_H_ */
 
 #endif
