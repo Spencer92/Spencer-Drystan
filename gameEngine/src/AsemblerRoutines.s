b2e32f34871ea2c2cf5f9e40c51a2044b6448616
Drystan <drystan.mazur@gmail.com>
2015-02-15 17:55:41 -0700

    working grapic demo

--- gameEngine/src/AsemblerRoutines.s
+++ gameEngine/src/AsemblerRoutines.s
@@ -9,10 +9,10 @@ SCREEN:		equ	 8
 
 
 _clear:		link	a6,#0
-			movea.l	SCREEN(a6),a0
-			move.l	#$1FFF,d1
+						movea.l	SCREEN(a6),a0
+						move.l	#$1FFF,d1
 
 clr:			clr.l	(a0)+
-			dbra	d1,clr
-			unlk	a6
-			rts
+					dbra	d1,clr
+					unlk	a6
+					rts
--- gameEngine/src/GameEngine.c
+++ gameEngine/src/GameEngine.c
@@ -46,118 +46,188 @@
 #define  NUMBER_TILES 1
 #define  MAXSPEED  5
 #define BUFFER_SIZE   0x8100L
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 400
 
 void gameReset();
 void pauseAndchill(int duration);
 
 int main() {
 
-UINT8 playerSprite[8] = { 0xff, 0x18, 0x18, 0xff, 0x18, 0xff, 0x18, 0xff };
-UINT8 enemySprite[8] = { 0xf1, 0xfd, 0xfd, 0x00, 0xff, 0x18, 0xfd, 0xff };
+	UINT8 playerSprite[8] = { 0xff, 0x18, 0x18, 0xff, 0x18, 0xff, 0x18, 0xff };
+	UINT8 enemySprite[8] = { 0xf1, 0xfd, 0xfd, 0x00, 0xff, 0x18, 0xfd, 0xff };
 
-/* TODO Screen *screenAray[2] = {};*/
+	/* TODO Screen *screenAray[2] = {};*/
 
+	/*playerTank.x_coordinate = 200;*/
 
-/*playerTank.x_coordinate = 200;*/
+	/*Tank enemyTank = { 300,300,ENEMY_HITPOINTS,MAXSPEED,TRUE,FALSE,enemySprite,NULL,-1,0,TRUE};*/
+	/*Missile *missiles[2] = { };*/
 
-/*Tank enemyTank = { 300,300,ENEMY_HITPOINTS,MAXSPEED,TRUE,FALSE,enemySprite,NULL,-1,0,TRUE};*/
-/*Missile *missiles[2] = { };*/
+	char keypress = 0;
 
-char keypress;
+	UINT16 playerScore = 0;
+	UINT8 i = 0;
 
-UINT16 playerScore = 0;
+	char *mainScreen;
+	char *logMainscreen;
+	char *gameScreen;
+	char *backGamescreen;
 
-char  *mainScreen;
-char  *gameScreen;
+	Tank playerTank = { 20, 50, PLAYER_HITPOINTS, MAXSPEED, TRUE, FALSE, NULL,
+			NULL, 1, 0, TRUE };
+	Tank enemyTank = { 600, 10, ENEMY_HITPOINTS, MAXSPEED, TRUE, FALSE, NULL,
+			NULL, -1, 0, TRUE };
 
-Tank playerTank; /*= {200,200,PLAYER_HITPOINTS,MAXSPEED,TRUE,FALSE,playerSprite,NULL,1,0,TRUE};*/
+	playerTank.sprite = playerSprite;
+	enemyTank.sprite = enemySprite;
 
-playerTank.x_coordinate = 200;
+	/*=========Will Change in next system=========================*/
+	mainScreen = (Physbase());
+	logMainscreen = (Logbase());
+	gameScreen = (char*) Malloc(BUFFER_SIZE);
+	gameScreen = (char*) ((UINT32) (gameScreen + 256) & 0x00FFFF00); /* The screens have to be 256 byte aligned */
 
-/*=========Will Change in next system=========================*/
-mainScreen =  (Physbase());
+	backGamescreen = (char*) Malloc(BUFFER_SIZE);
+	backGamescreen = (char*) ((UINT32) (gameScreen + 256) & 0x00FFFF00); /* The screens have to be 256 byte aligned */
+	clear(gameScreen);
+	clear(backGamescreen);
+	/*clear(mainScreen);*/
+	/*===========================================================*/
 
-gameScreen = (char*)Malloc(BUFFER_SIZE);
+	/* TODO Initiate game system */
 
-gameScreen = (char*)((UINT32)(gameScreen + 256) & 0x00FFFF00); /* The screens have to be 256 byte aligned */
+	plotSprite(gameScreen, playerTank.sprite, playerTank.x_coordinate,
+			playerTank.y_coordinate, 8);
+	plotSprite(gameScreen, enemyTank.sprite, enemyTank.x_coordinate,
+			enemyTank.y_coordinate, 8);
 
-clear(gameScreen);
-/*===========================================================*/
-
-/* TODO Initiate game system */
-/*
-
-plotSprite(gameScreen, playerTank->sprite, playerTank->x_coordinate,playerTank->y_coordinate, 8);
-plotSprite(gameScreen, enemyTank->sprite, enemyTank->x_coordinate, enemyTank->y_coordinate, 8);
-*/
-
-
-
-Cconws("\r\n\0");
-Cconws("On first screen \r\n\0");
-Cconws("Please press any key to start \r\n\0");
-
-while (!Cconis()) {
+	/*Setscreen(gameScreen,GameScreen,-1L);*/
 
 	Cconws("\r\n\0");
-	Cconws("Display start screen  here\r\n\0");
-	Cconws("Press space to start or q to quit \r\n\0");
-	Cconws("\r\n\0");
+	Cconws("On first screen \r\n\0");
+	Cconws("Please press any key to start \r\n\0");
 
-}
+	while (!Cconis()) {
 
-keypress = Cnecin();
-
-if(keypress != 'q')
-{
-/*=====================here is main game loop==================*/
-
-	do {
-
-
-
-		Cconws("\r\n\0");
-		Cconws("In main loop press p to pause and any other key to play \r\n\0");
-		Cconws("\r\n\0");
-
-
-		keypress = Cnecin();
-
-/*=====================here is pause game loop==================*/
-
-
-		if(keypress != 'p')
-		{
-
-			while (!Cconis())
-			{
-				Cconws("In pause game loop \r\n\0");
-				Cconws("Please press any key to start \r\n\0");
-
-			}
-
+		if (i == 0) {
+			Cconws("\r\n\0");
+			Cconws("Display start screen  here\r\n\0");
+			Cconws("Press space to start or q to quit \r\n\0");
+			Cconws("\r\n\0");
 		}
 
+		if (!(i >= 1)) {
+			i++;
+		}
 
-keypress = Cnecin();
+	}
 
-	}while(keypress != 'q');
+	keypress = Cnecin();
 
-}
+	if (keypress != 'q') {
+/*=====================here is main game loop==================*/
+					Cursconf(0,0); /* removes cusors*/
+		             Vsync();
+		             Vsync();
+					Setscreen(gameScreen, gameScreen, -1L);
 
 
-/*TODO clean up*/
+					do {
+						if(!(playerTank.x_coordinate  >= SCREEN_WIDTH ))
+						{
+							playerTank.x_coordinate += 5;
+
+							enemyTank.x_coordinate -= 5;
 
 
-return 0;
+						}
+						else
+						{
+							playerTank.x_coordinate = 20;
+
+							enemyTank.x_coordinate  = 600;
+						}
+
+
+						plotSprite(backGamescreen, playerTank.sprite,
+								playerTank.x_coordinate, playerTank.y_coordinate, 8);
+						plotSprite(backGamescreen, enemyTank.sprite, enemyTank.x_coordinate,
+								enemyTank.y_coordinate, 8);
+
+					/*	pauseAndchill(1);*/
+						Vsync();
+						Vsync();
+						Setscreen(backGamescreen, backGamescreen, -1L);
+						clear(gameScreen);
+
+
+						if(!(playerTank.x_coordinate  >= SCREEN_WIDTH ))
+						{
+							playerTank.x_coordinate += 5;
+
+							enemyTank.x_coordinate -= 5;
+
+
+						}
+						else
+						{
+							playerTank.x_coordinate = 20;
+
+							enemyTank.x_coordinate  = 600;
+						}
+
+						plotSprite(gameScreen, playerTank.sprite, playerTank.x_coordinate,
+								playerTank.y_coordinate, 8);
+						plotSprite(gameScreen, enemyTank.sprite, enemyTank.x_coordinate,
+								enemyTank.y_coordinate, 8);
+
+						Vsync();
+						Setscreen(gameScreen, gameScreen, -1L);
+						clear(backGamescreen);
+						/*pauseAndchill(1);*/
+
+
+
+						if (keypress == 'p') {
+							/*=====================here is pause game loop==================*/
+							clear(mainScreen);
+							Vsync();
+							Setscreen(logMainscreen, mainScreen, -1L);
+							Cconws("In pause game loop \r\n\0");
+							Cconws("Please press any key to start \r\n\0");
+
+							while (!Cconis()) {
+							}
+							keypress = Cnecin();
+						}
+
+						if(Cconis()){
+						keypress = Cnecin();}
+
+					} while (keypress != 'q');
+
+	}
+
+	/*TODO clean up*/
+
+	Mfree(gameScreen);
+	Mfree(backGamescreen);
+
+	return 0;
 
 }
 
 void pauseAndchill(int duration) {
 
-	/*TODO some kind of pause function*/
+	duration *= 35;
+
+	while (duration--) {
+		Vsync();
+	}
+
 }
 
 void gameReset() {
-/*TODO Here we need to reset all variables and game states*/
+	/*TODO Here we need to reset all variables and game states*/
 }
