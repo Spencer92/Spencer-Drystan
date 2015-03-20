ce470251d23532e965bcbae6656dd229658bace4
Spencer92 <smasl811@mtroyal.ca>
2015-03-20 17:04:43 -0600

    Integrated model.c into gameengine

--- gameEngine/src/Keyboard.c
+++ gameEngine/src/Keyboard.c
@@ -1,1 +1,22 @@
 /*Routines to read the keyboard directly*/
+
+void DSconout(char output)
+{
+	Cconout(output);
+}
+
+
+char DSnecin()
+{
+	return Cnecin();
+}
+
+BOOL DSconis()
+{
+	return Cconis();
+}
+
+void DSconws(String output)
+{
+	Cconws(output);
+}
